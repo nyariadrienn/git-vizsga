4665407ad10f6d7c6c3753ffd9c33072b088b19a
adrienn.j.nyari@gmail.com <adrienn.j.nyari@gmail.com>
2023-07-16 21:48:05 +0200

    app.js konzolüzenet beállítva

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    console.log("Oldal betöltődése kész.");
+};
+
 const musicContainer = document.getElementById("music-container");
 const playBtn = document.getElementById("play");
 const prevBtn = document.getElementById("prev");
